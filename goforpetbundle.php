c389da42f8a13a135a9d48d81e90a75e6e2cb3f0
Lucio Benini <iiriya@iiriya.com>
2021-04-18 20:22:22 +0200

    Update goforpetbundle.php

--- goforpetbundle.php
+++ goforpetbundle.php
@@ -20,7 +20,7 @@ class GoForPetBundle extends Module
     {
         $this->name          = 'goforpetbundle';
         $this->tab           = 'front_office_features';
-        $this->version       = '1.0.0';
+        $this->version       = '1.0.1';
         $this->author        = 'Go For Pet';
         $this->need_instance = 1;
 
@@ -42,26 +42,27 @@ class GoForPetBundle extends Module
 
     public function hookDisplayFooterProduct($params)
     {
-		$product  = $params['product'];
-		$related = $this->getRelatedProduct($product);
-		
-		if ($related) {
-			$factory = $this->getFactory();
-			
-			$this->smarty->assign('related', $factory->getPresenter()->present(
-				$factory->getPresentationSettings(),
-				$this->getProductPresenter($related),
-				$this->context->language
-			));
-			
-			return $this->display(__FILE__, 'views/templates/hook/displayFooterProduct.tpl');
-		} else {
-			return null;
-		}
+        $product  = $params['product'];
+        $related = $this->getRelatedProduct($product);
+
+        if ($related) {
+            $factory = $this->getFactory();
+
+            $this->smarty->assign('related', $factory->getPresenter()->present(
+                $factory->getPresentationSettings(),
+                $this->getProductPresenter($related),
+                $this->context->language
+            ));
+
+            return $this->display(__FILE__, 'views/templates/hook/displayFooterProduct.tpl');
+        } else {
+            return null;
+        }
     }
-	
-	protected function getRelatedProduct($product) {
-		$results = Db::getInstance()->executeS("
+
+    protected function getRelatedProduct($product)
+    {
+        $results = Db::getInstance()->executeS("
 			SELECT
 				cp.id_product
 			FROM `" . _DB_PREFIX_ . "cart` cart
@@ -72,7 +73,7 @@ class GoForPetBundle extends Module
 				FROM `" . _DB_PREFIX_ . "cart_product` cp
 				INNER JOIN `" . _DB_PREFIX_ . "product` product ON (product.id_product=cp.id_product)
 				" . Shop::addSqlAssociation('product', 'product') . "
-				WHERE product.id_product=" . pSQL($product->id) . "
+				WHERE product.id_product=" . pSQL($product->id) . " AND product.active = 1
 				GROUP BY cp.id_cart
 				ORDER BY count DESC
 			) r ON (r.id_cart=cart.id_cart)
@@ -80,9 +81,9 @@ class GoForPetBundle extends Module
 			ORDER BY count DESC
 			LIMIT 1
         ");
-		
-		if (empty($results)) {
-			$results = Db::getInstance()->executeS("
+
+        if (empty($results)) {
+            $results = Db::getInstance()->executeS("
 				SELECT product.id_product, SUM(od.product_quantity) as product_quantity
 				FROM `" . _DB_PREFIX_ . "product` product
 				" . Shop::addSqlAssociation('product', 'product') . "
@@ -91,47 +92,48 @@ class GoForPetBundle extends Module
 				INNER JOIN `" . _DB_PREFIX_ . "category` category_parallel ON (category_parallel.id_parent=category_parent.id_category AND category_parallel.id_category!=product.id_category_default)
 				LEFT JOIN `" . _DB_PREFIX_ . "order_detail` od ON (od.product_id = product.id_product AND od.product_id!=" . pSQL($product->id) . ")
 				LEFT JOIN `" . _DB_PREFIX_ . "orders` o ON od.id_order = o.id_order
+                WHERE product.active = 1
 				GROUP BY od.product_id
 				ORDER BY product_quantity DESC
 				LIMIT 1
 			");
-			
-			if (empty($results)) {
-				$results = Db::getInstance()->executeS("
+
+            if (empty($results)) {
+                $results = Db::getInstance()->executeS("
 					SELECT product.id_product, SUM(od.product_quantity) as product_quantity
 					FROM `" . _DB_PREFIX_ . "product` product
 					" . Shop::addSqlAssociation('product', 'product') . "
 					LEFT JOIN `" . _DB_PREFIX_ . "order_detail` od ON (od.product_id = product.id_product AND od.product_id!=" . pSQL($product->id) . ")
 					LEFT JOIN `" . _DB_PREFIX_ . "orders` o ON od.id_order = o.id_order
+                    WHERE product.active = 1
 					GROUP BY od.product_id
 					ORDER BY product_quantity DESC
 					LIMIT 1
 				");
-			}
-		}
-		
-		return !empty($results) ? new Product($results[0]['id_product'], true, $this->context->language->id) : null;
-	}
-	
-	protected function getProductPresenter($product) {
-		$presenter = new ObjectPresenter();
-		
-		$o = $presenter->present($product);
-		
+            }
+        }
+
+        return !empty($results) ? new Product($results[0]['id_product'], true, $this->context->language->id) : null;
+    }
+
+    protected function getProductPresenter($product)
+    {
+        $presenter = new ObjectPresenter();
+
+        $o = $presenter->present($product);
+
         $o['id_product'] = (int) $product->id;
         $o['out_of_stock'] = (int) $product->out_of_stock;
         $o['new'] = (int) $product->new;
         $o['ecotax'] = Tools::convertPrice((float) $o['ecotax'], $this->context->currency, true, $this->context);
 
         $full = Product::getProductProperties($this->context->language->id, $o, $this->context);
-		
-        $full['show_quantities'] = (bool) (
-            Configuration::get('PS_DISPLAY_QTIES')
+
+        $full['show_quantities'] = (bool) (Configuration::get('PS_DISPLAY_QTIES')
             && Configuration::get('PS_STOCK_MANAGEMENT')
             && $product->quantity > 0
             && $product->available_for_order
-            && !Configuration::isCatalogMode()
-        );
+            && !Configuration::isCatalogMode());
         $full['quantity_label'] = ($product->quantity > 1) ? $this->trans('Items', [], 'Shop.Theme.Catalog') : $this->trans('Item', [], 'Shop.Theme.Catalog');
 
         if ($full['unit_price_ratio'] > 0) {
@@ -139,7 +141,7 @@ class GoForPetBundle extends Module
         }
 
         $group_reduction = GroupReduction::getValueForProduct($product->id, (int) Group::getCurrent()->id);
-		
+
         if ($group_reduction === false) {
             $group_reduction = Group::getReduction((int) $this->context->cookie->id_customer) / 100;
         }
@@ -150,10 +152,10 @@ class GoForPetBundle extends Module
             $full['price'],
             $this->context->currency->precision
         );
-		
-		return $full;
-	}
-	
+
+        return $full;
+    }
+
     private function getFactory()
     {
         return new ProductPresenterFactory($this->context, new TaxConfiguration());
